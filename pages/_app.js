1315809870bc6bf884998c03999eef03334b9bb5
violetta_chernysheva <violetta_chernysheva@enot.work>
2024-12-05 19:31:10 +0300

    fix bags

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+
 import '../src/assets/styles/App.css';
 import '../src/components/Header/header.css';
 import '../src/components/Intro/intro.css';
@@ -10,7 +11,12 @@ import '../src/components/Contacts/contacts.css';
 import '../src/components/Footer/footer.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return <>
+       <link
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css"
+      />
+  <Component {...pageProps} />;
+  </> 
 }
-
 export default MyApp;
